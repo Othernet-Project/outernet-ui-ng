cdfb6eac4f55955857784af76adc6a119d6b5711
Ben Ennis <ben@outernet.is>
2015-05-21 13:42:14 -0700

    updated testing to run from root dir

--- testing/test.py
+++ testing/test.py
@@ -9,37 +9,37 @@ app = Bottle()
 
 @app.route('/')
 def index():
-    return template('templates/index.mako')
+    return template('testing/templates/index.mako')
 
 
 @app.route('/working-test')
 def working_test():
-    return template('templates/test.mako')
+    return template('testing/templates/test.mako')
 
 
 @app.route('/button-test')
 def button_test():
-    return template('templates/button-test.mako')
+    return template('testing/templates/button-test.mako')
 
 
 @app.route('/hyperlink-test')
 def hyperlink_test():
-    return template('templates/hyperlink-test.mako')
+    return template('testing/templates/hyperlink-test.mako')
 
 
 @app.route('/font-test')
 def font_test():
-    return template('templates/font-test.mako')
+    return template('testing/templates/font-test.mako')
 
 
 @app.route('/color-test')
 def color_test():
-    return template('templates/color-test.mako')
+    return template('testing/templates/color-test.mako')
 
 
 @app.route('/static/:path#.+#', name='static')
 def static(path):
-    return static_file(path, root='static')
+    return static_file(path, root='testing/static')
 
 
 run(app, reloader=True, host='localhost', port=8080)
